b5ef600e7579c0f7d675025ede5ed3503ff7f1b8
Ranald <ranald.mackay@live.co.uk>
2018-05-18 06:21:17 +0100

    Add Gulp for some magic

---
+++ gulpfile.js
@@ -0,0 +1,4 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var shell = require('gulp-shell'); // Allow shell commands to be run from Gulp
